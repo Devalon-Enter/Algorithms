b7fcba5e051bbae7ac93f12c7b52c2f1723a3d7e
Devalon-Enter <faberlorin@hotmail.com>
2022-10-19 12:43:03 +0000

    The pathchecker algorithm movement is completed

--- src/PathChecker.ts
+++ src/PathChecker.ts
@@ -4,16 +4,11 @@
 function pathFinder(probability: number) {
 
   let numTable: Array<Array<number>> = [];
-  let colorTable: Array<Array<string>> = [];
-  let black: number = 0;
-  let white: number = 0;
+  let colorTable: Array<Array<number>> = [];
   const size: number = 5;
   let countBlack: number = 0;
   let countWhite: number = 0;
-
-
-
-  //Hello there
+  let countPath: number = 1;
 
 // Definition of numTable and colorTable
   for (let row: number = 0; row < size; row++) {
@@ -27,38 +22,50 @@ function pathFinder(probability: number) {
 
       if (numTable[row][col] <= probability) {
         countBlack++        
-        colorTable[row][col] = "%";
+        colorTable[row][col] = 1;
       } else {
         countWhite++
-        colorTable[row][col] = "|";
+        colorTable[row][col] = -1;
       }
     }
   }
 
-  for (let row: number = 0; row < size; row++) {
-    for (let col: number = 0; col < size; col++) {
+  for (let col: number = 0; col < size; col++) {
+    
+    let b1 = (col > 0) && colorTable[0][col - 1];
+    console.log(col)
 
+    if (colorTable[0][col] === 1) {
+      if (b1 === countPath - 1) {
+        colorTable[0][col] = countPath--;
+        console.log("first iteration:", countPath);
+      }
+      colorTable[0][col] = countPath;
+      console.log("second iteration:", countPath);
+      countPath++;
+      console.log("count raised:", countPath, "\n");
+    }
+
+
+  }
+
+  for (let row: number = 1; row < size; row++) {
+    for (let col: number = 0; col < size; col++) {
+  
       let b0 = (row === 0) && colorTable[0][col];
       let b1 = (col > 0 && row > 0) && colorTable[row-1][col-1];
       let b2 = (row > 0) && colorTable[row-1][col];
       let b3 = (col < size - 1 && row > 0) && colorTable[row-1][col+1];
       let b4 = (col > 0) && colorTable[row][col-1];
 
-      if (colorTable[row][col] === "%" && (b0 || b1 || b2 || b3 || b4))
-      {
-        black++;
-      } else {
-        white++;
+      if (colorTable[row][col] >= 1 && (b1 || b2 || b3 || b4)) {
+
       }
-      
     }
   }
   
   console.table(numTable);
   console.table(colorTable);
-
-  console.log("Tiles counted by algorithm:", black);
-  console.log("Tiles counted by algorithm:", white, "\n");
 
   console.log("Tiles counted at initialization:",countBlack);
   console.log("Tiles counted at initialization:",countWhite, "\n");
