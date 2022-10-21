b9bb87a03855ff6c602349e7cdeb7abf7876aa3a
Devalon-Enter <faberlorin@hotmail.com>
2022-10-21 09:56:48 +0000

    PathChecker simulation added

--- out/PathChecker.js
+++ out/PathChecker.js
@@ -1,21 +1,29 @@
 // import Rand from 'rand-seed';
 // const rand = new Rand('afsdfas');
 function pathFinder(probability) {
+    // Init the visual tables
     var numTable = [];
     var colorTable = [];
-    var size = 5;
+    // Defines size of the table
+    var size = 10;
+    // Different counts for black and white
+    // as well for paths existing in first row
     var countBlack = 0;
     var countWhite = 0;
     var countPath = 1;
-    // Definition of numTable and colorTable
+    // Definition for the numTable and colorTable 2D arrays
     for (var row = 0; row < size; row++) {
+        // Creates a new array with length of size => size ~10?
         numTable.push(new Array(size));
         colorTable.push(new Array(size));
         for (var col = 0; col < size; col++) {
+            // Assigns for numTable random number between 0 and 1
             numTable[row][col] = Math.round(Math.random() * 100) / 100;
+            // If the assigned number in the tile exceeds the probability
+            // it will be marked as white otherwise black
             if (numTable[row][col] <= probability) {
                 countBlack++;
-                colorTable[row][col] = 1;
+                colorTable[row][col] = 0;
             }
             else {
                 countWhite++;
@@ -23,52 +31,77 @@ function pathFinder(probability) {
             }
         }
     }
+    // Iterates through the first row of 2D array
+    // To check for potential paths
     for (var col = 0; col < size; col++) {
+        // Describes the movement pattern in first row
         var b1 = (col > 0) && colorTable[0][col - 1];
-        console.log(col);
-        if (colorTable[0][col] === 1) {
+        // In case of finding a tile with a number of 1
+        // assing it a new path number if not connected to already existing path
+        if (colorTable[0][col] === 0) {
             if (b1 === countPath - 1) {
                 colorTable[0][col] = countPath--;
-                console.log("first iteration:", countPath);
             }
             colorTable[0][col] = countPath;
-            console.log("second iteration:", countPath);
             countPath++;
-            console.log("count raised:", countPath, "\n");
         }
     }
-    for (var row = 0; row < size; row++) {
+    // Describes movement in 2D Array and checking for paths
+    for (var row = 1; row < size; row++) {
         for (var col = 0; col < size; col++) {
             if (colorTable[row][col] == -1)
                 continue;
-            var b0 = (row === 0 && colorTable[0][col]) ? colorTable[0][col] : -1;
+            // To check if tile is occupied by specific Path
+            // These variables hold diffrent moving patterns in the 2D array
             var b1 = (col > 0 && row > 0) ? colorTable[row - 1][col - 1] : -1;
             var b2 = (row > 0) ? colorTable[row - 1][col] : -1;
             var b3 = (col < size - 1 && row > 0) ? colorTable[row - 1][col + 1] : -1;
             var b4 = (col > 0) ? colorTable[row][col - 1] : -1;
-            var mypath = Math.max(b0, b1, b2, b3, b4);
+            // Checks for the highest number found in the movement
+            var mypath = Math.max(b1, b2, b3, b4);
+            // Assigns that highest number to current tile
             colorTable[row][col] = mypath;
-            // let c1 = colorTable[row-1][col-1];
-            // let c2 = colorTable[row-1][col];
-            // let c3 = colorTable[row-1][col+1];
-            // let c4 = colorTable[row][col-1];
-            // if (colorTable[row][col] >= 1 && (b0 || b1 || b2 || b3 || b4)) {
-            //   let location: Array<number> = []
-            //   location.push(c1, c2, c3, c4);
-            //   console.log(colorTable[row][col])
-            //   colorTable[row][col] = location;
-            // }
+            if (colorTable[size - 1][col] >= 1) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
     console.table(numTable);
     console.table(colorTable);
     console.log("Tiles counted at initialization:", countBlack);
     console.log("Tiles counted at initialization:", countWhite, "\n");
-    console.log("% = Black");
-    console.log("| = White \n");
+    console.log("1 or higher = Black");
+    console.log("-1 = White \n");
     var probabilityCheck = countBlack / (size * size);
     console.log("Probability Check:", probabilityCheck);
 }
 ;
-pathFinder(0.5);
+// function for different samples => creates different probabilitys
+function linspace(start, stop, num, endpoint) {
+    if (endpoint === void 0) { endpoint = true; }
+    var div = endpoint ? (num - 1) : num;
+    var step = (stop - start) / div;
+    return Array.from({ length: num }, function (_, i) { return start + step * i; });
+}
+var probs = linspace(0, 1, 11);
+var num_repetitions = 10;
+var ys = [];
+for (var _i = 0, probs_1 = probs; _i < probs_1.length; _i++) {
+    var p = probs_1[_i];
+    var path_found_count = 0;
+    for (var i = 0; i < num_repetitions; i++) {
+        if (pathFinder(p)) {
+            console.log(p);
+            path_found_count++;
+            console.log("Path found:", path_found_count);
+        }
+    }
+    var y = path_found_count / num_repetitions;
+    ys.push(y);
+    console.log(p.toString() + "," + y.toString());
+    console.log(path_found_count);
+}
 //# sourceMappingURL=PathChecker.js.map
--- src/PathChecker.ts
+++ src/PathChecker.ts
@@ -3,26 +3,36 @@
 
 function pathFinder(probability: number) {
 
+  // Init the visual tables
   let numTable: Array<Array<number>> = [];
   let colorTable: Array<Array<number>> = [];
-  const size: number = 10;
+
+  // Defines size of the table
+  const size: number = 100;
+
+  // Different counts for black and white
+  // as well for paths existing in first row
   let countBlack: number = 0;
   let countWhite: number = 0;
   let countPath: number = 1;
 
-// Definition of numTable and colorTable
+// Definition for the numTable and colorTable 2D arrays
   for (let row: number = 0; row < size; row++) {
 
+    // Creates a new array with length of size => size ~10?
      numTable.push(new Array(size))
      colorTable.push(new Array(size))
   
     for (let col:number = 0; col < size; col++) {
       
+      // Assigns for numTable random number between 0 and 1
       numTable[row][col]  = Math.round(Math.random() * 100) / 100;
 
+      // If the assigned number in the tile exceeds the probability
+      // it will be marked as white otherwise black
       if (numTable[row][col] <= probability) {
         countBlack++        
-        colorTable[row][col] = 1;
+        colorTable[row][col] = 0;
       } else {
         countWhite++
         colorTable[row][col] = -1;
@@ -30,52 +40,85 @@ function pathFinder(probability: number) {
     }
   }
 
+
+  // Iterates through the first row of 2D array
+  // To check for potential paths
   for (let col: number = 0; col < size; col++) {
     
+    // Describes the movement pattern in first row
     let b1 = (col > 0) && colorTable[0][col - 1];
-    console.log(col)
-
-    if (colorTable[0][col] === 1) {
+  
+    // In case of finding a tile with a number of 1
+    // assing it a new path number if not connected to already existing path
+    if (colorTable[0][col] === 0) {
       if (b1 === countPath - 1) {
         colorTable[0][col] = countPath--;
-        console.log("first iteration:", countPath);
       }
       colorTable[0][col] = countPath;
-      console.log("second iteration:", countPath);
       countPath++;
-      console.log("count raised:", countPath, "\n");
     }
-
-
   }
 
+
+  // Describes movement in 2D Array and checking for paths
   for (let row: number = 1; row < size; row++) {
     for (let col: number = 0; col < size; col++) {
+
       if (colorTable[row][col] == -1) continue;
 
+      // To check if tile is occupied by specific Path
+      // These variables hold diffrent moving patterns in the 2D array
       let b1 = (col > 0 && row > 0) ? colorTable[row-1][col-1] : -1;
       let b2 = (row > 0) ? colorTable[row-1][col] : -1;
       let b3 = (col < size - 1 && row > 0) ? colorTable[row-1][col+1] : -1;
       let b4 = (col > 0) ? colorTable[row][col-1] : -1;
 
+      // Checks for the highest number found in the movement
       let mypath = Math.max(b1, b2, b3, b4)
       
+      // Assigns that highest number to current tile
       colorTable[row][col] = mypath;
     }
   }
-  
-  console.table(numTable);
-  console.table(colorTable);
 
-  console.log("Tiles counted at initialization:",countBlack);
-  console.log("Tiles counted at initialization:",countWhite, "\n");
-
-  console.log("% = Black");
-  console.log("| = White \n");
-
-  let probabilityCheck: number = countBlack / (size * size);
-
-  console.log("Probability Check:",probabilityCheck)
+  // Checks the very last row for paths; higher than 0 => path true
+  for (let row: number = 0; row < size; row++) {
+    for (let col: number = 0; col < size; col++) {
+      if (colorTable[size-1][col] >= 1) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
 };
 
-pathFinder(0.2);
+// function for different samples => creates different probabilitys
+function linspace(start: number, stop: number, num: number, endpoint = true) {
+  const div = endpoint ? (num - 1) : num;
+  const step = (stop - start) / div;
+  return Array.from({length: num}, (_, i) => start + step * i);
+}
+
+// sample range definition; Range 0 to 1 and 11 samples
+const probs: Array<number> = linspace(0, 1, 11)
+
+// The amount of repetitions of the simulation
+const num_repetitions = 100;
+const ys: Array<number> = []
+
+// Runs the simulation and counts the paths found
+for (const p of probs) {
+
+  let path_found_count = 0;
+
+  for (let i: number = 0; i < num_repetitions; i++) {
+    if (pathFinder(p)) {
+      path_found_count++
+    }
+  }
+
+  const y = path_found_count / num_repetitions;
+  ys.push(y)
+  console.log(p.toString() + "," + y.toString())
+}
